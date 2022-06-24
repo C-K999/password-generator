a115a89dea66202b1ae5dce819439d937815eda1
C K <ctkoo@ucsc.edu>
2022-06-23 21:01:04 -0700

    the code should be working

--- Develop/script.js
+++ Develop/script.js
@@ -27,25 +27,50 @@ function writePassword() {
     if(passLength >= 8 && passLength <= 128){
 
         var lowerCase = confirm("First of all, would you like to include lowercase letters?");
-        //var upperCase = confirm("It shall be done. What about uppercases?")
-        //var numCase = confirm("I see. Would you like to include numbers?");
-        //var specialCase = confirm("Lastly, would you like your password to have special characters?\n"+"Special characters includes !#$%&'()*+,-./:;<=>?@[]^_`{|}~");
+        var upperCase = confirm("It shall be done. What about uppercases?");
+        var numCase = confirm("I see. Would you like to include numbers?");
+        var specialCase = confirm("Lastly, would you like your password to have special characters?\n"+"Special characters includes !#$%&'()*+,-./:;<=>?@[]^_`{|}~");
 
         var passwordProg = ["dummy"];
         for (var i = 0; i < passLength; i++) {
+
+            var authorize = ["dummy"];
             var randomAlpha = codebaseAlpha[randomNo(0,25)];
-            passwordProg.unshift(randomAlpha);
-            console.log("The amount of times this loop ran: "+(i+1));
+
+            if(lowerCase==true){
+                authorize.unshift(randomAlpha);
+            }
+
+            if(upperCase==true){
+                authorize.unshift(randomAlpha.toUpperCase());
+            }
+
+            if(numCase==true){
+                authorize.unshift(randomNo(0,9));
+            }
+
+            if(specialCase==true){
+                authorize.unshift(codebaseSpecial[randomNo(0,codebaseSpecial.length-1)]);
+            }
+
+            if(lowerCase==false && upperCase==false && specialCase==false && numCase==false){
+                alert("Your specified password cannot be generated, please try again.");
+                return(generatePassword());
+            }
+            authorize.pop();
+            
+            passwordProg.unshift(authorize[randomNo(0,authorize.length-1)]);
+            
         }
         passwordProg.pop();
-        console.log(passwordProg.join(""));
+        return(passwordProg.join(""));
 
     }else{
-        alert("Your specified length is either too long or too short, please enter another number.");
+        alert("Please enter a valid number.");
         return(generatePassword());
     }
-    
-}
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
